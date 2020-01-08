b4ecbc5bb59f66f357cfefff3d34936cf2993220
Seth Shikora <seth@shikora.org>
2020-01-07 18:21:17 -0800

    forgot to add the client file

---
+++ src/Client.js
@@ -0,0 +1,27 @@
+
+function getWeather(lat, long) {
+  return fetch(`https://api.darksky.net/forecast/${config.token}/${lat},${long}`, {
+    accept: "application/json"
+  })
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(weather)
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`);
+  error.status = response.statusText;
+  error.response = response;
+  console.log(error); // eslint-disable-line no-console
+  throw error;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+const Client = { getWeather };
+export default Client;
